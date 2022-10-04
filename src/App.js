048b33ff3485475ba33007f22e366dbb1cc331d6
Aman singh Rajput <rajput.aman900@gmail.com>
2022-10-05 04:14:27 +0530

    deploy my react app to github pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+//import ExpenseDate from './ExpenseDate.js';
+//sample data
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2',
+   title: 'New TV',
+   amount: 799.49,
+   date: new Date(2021, 2, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+  {
+    id: 'e5',
+    title: 'Toys',
+    amount: 45,
+    date: new Date(2019, 5, 12),
+  },
+  {
+    id: 'e6',
+    title: 'Food',
+    amount: 100,
+    date: new Date(2019, 4, 10),
+  },
+  {
+    id: 'e7',
+    title: 'Hair Dryer',
+    amount: 210,
+    date: new Date(2022, 2, 10),
+  },
+  {
+    id: 'e8',
+    title: 'iPhone',
+    amount: 799,
+    date: new Date(2022, 4, 15),
+  },
+  {
+    id: 'e9',
+    title: 'Laptop',
+    amount: 1500,
+    date: new Date(2020, 9, 18),
+  },
+  {
+    id: 'e10',
+    title: 'House Rent',
+    amount: 950,
+    date: new Date(2019, 10, 1),
+  },
+];
 
-function App() {
+const App = () => {
+
+    const [expenses, setExpenses] = useState(DUMMY_EXPENSES)
+
+  const addExpenseHandler = expense => {
+    setExpenses(prevExpenses => {
+      return [expense, ...prevExpenses];
+    });
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler}/>
+      <Expenses items={expenses}/>
     </div>
   );
 }
